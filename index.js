b97e722a88875f71c6a17d7e3d85cff63690e79c
Carlos Misachi <calphonce31@gmail.com>
2022-06-21 13:20:37 +0300

    updates

--- index.js
+++ index.js
@@ -17,18 +17,18 @@
 // }
 document.getElementById("btn").onclick =()=>{
     var location = document.getElementById("location").value;
-    var icon  =  document.getElementById("icon").value;
+    var icon  =  document.getElementById("icon")
     var weather  = document.getElementById("weather").value; 
 
 
 
 
-    fetch("http://api.weatherapi.com/v1/current.json?key=cdfc1ecfa8294e34b64154938221406&q="+ 'Nairobi' +"&aqi=no")
+    fetch("http://api.weatherapi.com/v1/current.json?key=cdfc1ecfa8294e34b64154938221406&q="+location+"&aqi=no")
 .then(querySnapshot => querySnapshot.json())
 .then(currentWeather => {
 console.log(currentWeather);
 document.getElementById("weather").innerHTML = currentWeather.current.condition.text; 
-document.getElementById("icon").src = currentWeather.current.condition.icon;
+icon.src = currentWeather.current.condition.icon;
 document.getElementById("locationn").innerHTML = currentWeather.location.name;
 document.getElementById("temp").innerHTML = `${currentWeather.current.temp_c}C`;
 document.getElementById("date").innerHTML = currentWeather.location.localtime;
